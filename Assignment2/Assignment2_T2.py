7063d72cf7a8fe413d62c7fcb2410ccb4e66c31e
trilokimodi <55188462+trilokimodi@users.noreply.github.com>
2020-11-20 02:59:01 +0100

    Task 2 completed with outputs. One special can be added if necessary

--- Assignment2/Assignment2_T2.py
+++ Assignment2/Assignment2_T2.py
@@ -1,5 +1,8 @@
 from collections import Counter
 import numpy as np
+from spacy.lang.en import English
+
+nlp = English()
 
 
 class Corpus:
@@ -10,28 +13,33 @@ class Corpus:
         self.docLen = list()
         self.numDocs = 0
         docNumber = 0
+        numActualToken = 0
         fileHandler = open(filePath, encoding=encoding)
         with fileHandler:
             for doc in fileHandler:
                 self.numDocs += 1
-                self.docLen.append(len(doc))
                 tokens = doc.lower().split()
-                self.bagOfWords.update(tokens)
-                self.listOfWords = self.listOfWords + tokens
-                tempArray = docNumber * np.ones(len(tokens), dtype=int)
+                numActualToken += len(tokens)
+                numTokenTemp = 0
+                for iToken in tokens:
+                    if not nlp.vocab[iToken].is_stop and iToken not in ['.', '&', '...', '-', ';', '/', '!', ',', '(',
+                                                                        ')', "'", '"', ':', "?",
+                                                                        '....'] and not iToken.isdigit():
+                        self.bagOfWords[iToken] += 1
+                        self.listOfWords = self.listOfWords + [iToken]
+                        numTokenTemp += 1
+                self.docLen.append(numTokenTemp)
+                tempArray = docNumber * np.ones(numTokenTemp, dtype=int)
                 self.wordInDocIndex = np.concatenate((self.wordInDocIndex, tempArray), axis=None)
                 docNumber += 1
-                if len(self.listOfWords) >= maxTokens + 1:
+                if numActualToken >= maxTokens + 1:
                     self.listOfWords.pop(0)  # Removed "removeMe"
                     self.wordInDocIndex = np.delete(self.wordInDocIndex, 0)
                     break
         fileHandler.close()
 
-    def most_common_word(self, numOfMostCommonWords, needToStore):
-        if needToStore == 'Y':
-            self.corpusMostCommonWords = Counter(self.bagOfWords).most_common(numOfMostCommonWords)
-        elif needToStore == 'N':
-            print(Counter(self.bagOfWords).most_common(numOfMostCommonWords))
+    def most_common_word(self, numOfMostCommonWords):
+        self.corpusMostCommonWords = Counter(self.bagOfWords).most_common(numOfMostCommonWords)
 
 
 class IntegerVocabulary:
@@ -61,7 +69,7 @@ filePath = "D:\\Masters Program Chalmers\\Projects and Labs\\MLNLP\\Assignment1\
 fileEncoding = "ISO-8859-1"
 
 maxGibbsIterations = 200
-maxTokens = 50000
+maxTokens = 100000
 books = Corpus(filePath, fileEncoding, maxTokens)
 maxTokens = len(books.listOfWords)
 numUniqueWords = len(books.bagOfWords)
@@ -69,91 +77,116 @@ numDocs = len(books.docLen)
 
 maxVocabSize = numUniqueWords
 numOfMostCommonWords = maxVocabSize  # Not considering padding and out-of-vocabulary - See assignment 1
-books.most_common_word(numOfMostCommonWords, 'Y')
+books.most_common_word(numOfMostCommonWords)
 booksIV = IntegerVocabulary(books.corpusMostCommonWords, maxVocabSize)
 
-numTopics = 5
-dirParameter = 0.1
-topicParameter = 0.1
+numTopicsList = [5, 10]
+parameterList = [(0.1, 0.1), (0.01, 0.01)]
 
-bigramTopicFreq = np.zeros((numUniqueWords, numUniqueWords, numTopics), dtype=int)  # This is transpose of what it
-# suppose to be
-wordTopicFreq = np.zeros((numUniqueWords, numTopics), dtype=int)
-wordTopic = np.random.randint(0, numTopics, maxTokens)
+for iTopicList in range(len(numTopicsList)):
+    for iParameterList in range(len(parameterList)):
+        numTopics = numTopicsList[iTopicList]
+        dirParameter = parameterList[iParameterList][0]
+        topicParameter = parameterList[iParameterList][1]
 
-docId = np.arange(0, numDocs, 1)
-docTopicFreq = np.zeros((numDocs, numTopics), dtype=int)
+        bigramTopicFreq = np.zeros((numUniqueWords, numUniqueWords, numTopics),
+                                   dtype=int)  # This is transpose of what it suppose to be
+        wordTopicFreq = np.zeros((numUniqueWords, numTopics), dtype=int)
+        wordTopic = np.random.randint(0, numTopics, maxTokens)
 
-# Random initialization matrix updates
-jDocId = 0
-for iNumber, iWord in enumerate(books.listOfWords[1:]):
-    wordIdentity = booksIV.integerVocab[iWord]
-    prevWord = books.listOfWords[iNumber - 1]
-    prevWordIdentity = booksIV.integerVocab[prevWord]
-    jDocId = books.wordInDocIndex[iNumber]
-    jDocIdPrevWord = books.wordInDocIndex[iNumber - 1]
-    if jDocId == jDocIdPrevWord:
-        bigramTopicFreq[wordIdentity, prevWordIdentity, wordTopic[iNumber]] += 1
-        wordTopicFreq[wordIdentity, wordTopic[iNumber]] += 1
-        docTopicFreq[jDocId, wordTopic[iNumber]] += 1
+        docId = np.arange(0, numDocs, 1)
+        docTopicFreq = np.zeros((numDocs, numTopics), dtype=int)
 
-iGibbs = 0
+        # Random initialization matrix updates
+        jDocId = 0
+        for iNumber, iWord in enumerate(books.listOfWords[1:]):
+            wordIdentity = booksIV.integerVocab[iWord]
+            prevWord = books.listOfWords[iNumber - 1]
+            prevWordIdentity = booksIV.integerVocab[prevWord]
+            jDocId = books.wordInDocIndex[iNumber]
+            jDocIdPrevWord = books.wordInDocIndex[iNumber - 1]
+            if jDocId == jDocIdPrevWord:
+                bigramTopicFreq[wordIdentity, prevWordIdentity, wordTopic[iNumber]] += 1
+                wordTopicFreq[wordIdentity, wordTopic[iNumber]] += 1
+                docTopicFreq[jDocId, wordTopic[iNumber]] += 1
 
-while iGibbs < maxGibbsIterations:
-    print(iGibbs)
-    iGibbs += 1
-    iDocId = 0
-    iDocIdPrevWord = 0
-    for iNumber, iWord in enumerate(books.listOfWords[1:]):
-        topicNumber = wordTopic[iNumber]
-        wordIdentity = booksIV.integerVocab[iWord]
-        prevWord = books.listOfWords[iNumber - 1]
-        prevWordIdentity = booksIV.integerVocab[prevWord]
-        iDocId = books.wordInDocIndex[iNumber]
-        iDocIdPrevWord = books.wordInDocIndex[iNumber - 1]
-        if iDocId == iDocIdPrevWord:
-            bigramTopicFreq[wordIdentity, prevWordIdentity, topicNumber] -= 1
-            wordTopicFreq[wordIdentity, topicNumber] -= 1
-            docTopicFreq[iDocId, topicNumber] -= 1
-            docTopicProb = estimateDocTopicProb(iDocId)
-            wordTopicProb = estimateTopicWordProb(wordIdentity,
-                                                  prevWordIdentity)  # Notice we have passed the integer index
-            probWordInToken = np.multiply(docTopicProb, wordTopicProb)
-            selectedTopic = np.random.multinomial(1, probWordInToken / probWordInToken.sum()).argmax()
-            bigramTopicFreq[wordIdentity, prevWordIdentity, selectedTopic] += 1
-            docTopicFreq[iDocId, selectedTopic] += 1
-            wordTopicFreq[wordIdentity, selectedTopic] += 1
-            wordTopic[iNumber] = selectedTopic
+        iGibbs = 0
 
-topicWordRelationByRawCount = list()
-topicWordRelationByRelativeCount = list()
-for iTopic in range(numTopics):
-    topicWordRelationByRawCount.append(Counter())
+        while iGibbs < maxGibbsIterations:
+            print(iGibbs)
+            iGibbs += 1
+            iDocId = 0
+            iDocIdPrevWord = 0
+            for iNumber, iWord in enumerate(books.listOfWords[1:]):
+                topicNumber = wordTopic[iNumber]
+                wordIdentity = booksIV.integerVocab[iWord]
+                prevWord = books.listOfWords[iNumber - 1]
+                prevWordIdentity = booksIV.integerVocab[prevWord]
+                iDocId = books.wordInDocIndex[iNumber]
+                iDocIdPrevWord = books.wordInDocIndex[iNumber - 1]
+                if iDocId == iDocIdPrevWord:
+                    bigramTopicFreq[wordIdentity, prevWordIdentity, topicNumber] -= 1
+                    wordTopicFreq[wordIdentity, topicNumber] -= 1
+                    docTopicFreq[iDocId, topicNumber] -= 1
+                    docTopicProb = estimateDocTopicProb(iDocId)
+                    wordTopicProb = estimateTopicWordProb(wordIdentity,
+                                                          prevWordIdentity)  # Notice we have passed the integer index
+                    probWordInToken = np.multiply(docTopicProb, wordTopicProb)
+                    selectedTopic = np.random.multinomial(1, probWordInToken / probWordInToken.sum()).argmax()
+                    bigramTopicFreq[wordIdentity, prevWordIdentity, selectedTopic] += 1
+                    docTopicFreq[iDocId, selectedTopic] += 1
+                    wordTopicFreq[wordIdentity, selectedTopic] += 1
+                    wordTopic[iNumber] = selectedTopic
 
-for iNumber, iWord in enumerate(books.listOfWords):
-    topicWordRelationByRawCount[wordTopic[iNumber]][iWord] += 1
+        topicWordRelationByRawCount = list()
+        topicWordRelationByRelativeCount = list()
+        for iTopic in range(numTopics):
+            topicWordRelationByRawCount.append(Counter())
 
-for iTopic in range(numTopics):
-    topicWordRelationByRelativeCount.append(topicWordRelationByRawCount[iTopic].copy())
+        for iNumber, iWord in enumerate(books.listOfWords):
+            topicWordRelationByRawCount[wordTopic[iNumber]][iWord] += 1
 
-for iTopic in range(numTopics):
-    for iWord in topicWordRelationByRawCount[iTopic].keys():
-        temp = topicWordRelationByRawCount[iTopic][iWord]
-        topicWordRelationByRelativeCount[iTopic][iWord] = temp / books.bagOfWords[
-            iWord]
+        for iTopic in range(numTopics):
+            topicWordRelationByRelativeCount.append(topicWordRelationByRawCount[iTopic].copy())
 
-for iTopic in range(numTopics):
-    topicWordRelationByRawCount[iTopic] = sorted(topicWordRelationByRawCount[iTopic].items(), key=lambda x: x[1],
-                                                 reverse=True)
-    topicWordRelationByRelativeCount[iTopic] = sorted(topicWordRelationByRelativeCount[iTopic].items(),
-                                                      key=lambda x: x[1], reverse=True)
+        for iTopic in range(numTopics):
+            for iWord in topicWordRelationByRawCount[iTopic].keys():
+                temp = topicWordRelationByRawCount[iTopic][iWord]
+                topicWordRelationByRelativeCount[iTopic][iWord] = temp / books.bagOfWords[
+                    iWord]
 
-numWordsToPrint = 30
-for iTopic in range(numTopics):
-    print("Topic number = %d, number of words in it = %d" % (iTopic, len(topicWordRelationByRelativeCount[iTopic])))
-    for x in range(numWordsToPrint):
-        print(topicWordRelationByRawCount[iTopic][x][0], end="\t")
-    print("")
-    for x in range(numWordsToPrint):
-        print(topicWordRelationByRelativeCount[iTopic][x][0], end="\t")
-    print("\n")
+        for iTopic in range(numTopics):
+            topicWordRelationByRawCount[iTopic] = sorted(topicWordRelationByRawCount[iTopic].items(),
+                                                         key=lambda x: x[1],
+                                                         reverse=True)
+            topicWordRelationByRelativeCount[iTopic] = sorted(topicWordRelationByRelativeCount[iTopic].items(),
+                                                              key=lambda x: x[1], reverse=True)
+
+        numWordsToPrint = 50
+        # for iTopic in range(numTopics):
+        #     print("Topic number = %d, number of words in it = %d" % (
+        #         iTopic, len(topicWordRelationByRelativeCount[iTopic])))
+        #     for x in range(numWordsToPrint):
+        #         print(topicWordRelationByRawCount[iTopic][x][0], end="\t")
+        #     print("")
+        #     for x in range(numWordsToPrint):
+        #         print(topicWordRelationByRelativeCount[iTopic][x][0], end="\t")
+        #     print("\n")
+
+        filePath2 = "D:\\Masters Program Chalmers\\Projects and Labs\\MLNLP\\Assignment1\\a1_data\\A2_Task2.txt"
+        fileHandler2 = open(filePath2, 'a')
+        with fileHandler2:
+            fileHandler2.write("K = %s, alpha = beta = %s\n" % (str(numTopics), str(dirParameter)))
+            for iTopic in range(numTopics):
+                fileHandler2.write("\n\nTopic number = %s, number of words in it = %s\n" % (
+                    str(iTopic), str(len(topicWordRelationByRelativeCount[iTopic]))))
+                fileHandler2.write("\nBy raw count\n")
+                for x in range(numWordsToPrint):
+                    fileHandler2.write(topicWordRelationByRawCount[iTopic][x][0])
+                    fileHandler2.write("\t")
+                fileHandler2.write("\nBy relative count\n")
+                for x in range(numWordsToPrint):
+                    fileHandler2.write(topicWordRelationByRelativeCount[iTopic][x][0])
+                    fileHandler2.write("\t")
+            fileHandler2.write("\n\n\n")
+        fileHandler2.close()
